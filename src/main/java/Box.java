ed049eb6eefe7ca7c12102bbfaaae812e1755796
Azamat <azamattemirzhan@gmail,com>
2021-02-17 22:22:09 +0300

    zxczxc

--- src/main/java/Box.java
+++ src/main/java/Box.java
@@ -1,2 +1,49 @@
-public class Box {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+public class Box<T extends Fruit> {
+    ArrayList<T> box;
+
+    public Box() {
+        box = new ArrayList<>();
+    }
+
+    public Box(T fruit) {
+        box = new ArrayList<>();
+        box.add(fruit);
+    }
+
+    public boolean compare(Box<?> box){
+        if(this.getWeight()>box.getWeight())
+            return true;
+        return false;
+    }
+
+    public float getWeight(){
+        float weight = 0.0f;
+        for (T s: box) {
+            weight+= s.weight;
+        }
+        return weight;
+    }
+    public void addFruit(Box<T> box){
+        try {
+            this.box.addAll((Collection<? extends T>) box);
+            ((Collection<?>) box).clear();
+        }catch (ClassCastException e){
+            System.out.println("нельзя перемешивать");
+        }
+
+    }
+    public void addFruit(T  fruit)throws ClassCastException{
+        try {
+        this.box.add(fruit);
+        ((Collection<?>) box).clear();
+        }catch (ClassCastException e){
+            System.out.println("нельзя перемешивать");
+        }
+    }
+
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,13 @@
 public class Main {
+    public static void main(String[] args) {
+        Box<Apple> firstBox = new Box<>(new Apple());
+        Box<Orange> secondBox = new Box<>(new Orange());
+        Apple a = new Apple();
+        System.out.println(firstBox.compare(secondBox));
+
+           secondBox.addFruit(secondBox);
+
+
+
+    }
 }
